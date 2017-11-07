af3cb5cdf489e90f8097df054ae74b7c99a5a530
SAU <santiago.alarcon@bbva.com>
2017-11-07 23:51:00 +0100

    base_curso_preparada

--- curso-solutions.py
+++ curso-solutions.py
@@ -1,9 +1,12 @@
 import webapp2
+import os
+from google.appengine.ext.webapp import template
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hola Solutions Architects 03102017')
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, {}))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
